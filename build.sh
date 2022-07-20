a4c617ae50e5b906fdc15e67f6d4bd5209e44021
dgiorgio <douglas.giorgio@gmail.com>
2022-07-20 00:21:58 -0300

    update alpine, add tini

--- build.sh
+++ build.sh
@@ -4,22 +4,23 @@ set -e
 
 cd ./build
 
-# default - vars
-[ -z "${BUILD}" ] && BUILD=""
-
 # build gvm-feed-server
+gvm_feed_server_version=2.2
+build_gvm_feed_server="-20220720"
 echo "
 ################################################################################
-################### Build gvm-feed-server ##############################################
-################################################################################"
-gvm_feed_server_version=2.1
-build_gvm_feed_server=""
+################### Build gvm-feed-server ######################################
+################################################################################
+Image: ${REGISTRY}dgiorgio/gvm-feed-server:${gvm_feed_server_version}${build_gvm_feed_server}"
+
 docker build -f ./Dockerfile \
-  -t "dgiorgio/gvm-feed-server:${gvm_feed_server_version}${build_gvm_feed_server:-${BUILD}}" \
+  -t "dgiorgio/gvm-feed-server:${gvm_feed_server_version}${build_gvm_feed_server}" \
+  -t "dgiorgio/gvm-feed-server:${gvm_feed_server_version}" \
   -t "dgiorgio/gvm-feed-server:latest" .
 
 # push
 if [ "${1}" == "push" ]; then
-  docker push "dgiorgio/gvm-feed-server:${gvm_feed_server_version}${build_gvm_feed_server:-${BUILD}}"
+  docker push "dgiorgio/gvm-feed-server:${gvm_feed_server_version}${build_gvm_feed_server}"
+  docker push "dgiorgio/gvm-feed-server:${gvm_feed_server_version}"
   docker push "dgiorgio/gvm-feed-server:latest"
 fi
